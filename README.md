36ad56a6892717fbd7dfb8ec19f5a79b484fc588
Kwonelee <995546054@qq.com>
2025-09-25 22:47:41 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# iStoreOS-NDK
+
+# 🤔Introduce
+> **🔈 本项目旨在将 RK3399 平台适配至 iStoreOS 官方源码，并通过原生编译构建 `sysupgrade` 格式固件**
+
+> **🔈 RK3399 算是移植中的一块“硬骨头”，只要把它啃下来，其他机型的移植工作基本上就能水到渠成了**
+
+> **😂如果您在适配其他 RK 机型时遇到困难，也欢迎您直接使用这个项目😂👉👉👉[iStoreOS-Actions](https://github.com/Kwonelee/iStoreOS-Actions/releases)**
+
+> **使用此仓库必须设置机密token，Actions云编译固件时需要用到，通常在仓库设置里面。机密键名为 `GH_TOKEN`（严禁在仓库可视代码中填写，否则后果自负）**
+
+# 😅Built-in package plan
+| 插件                     | 状态 | 源码地址                                                                   | 备注         |
+|:------------------------:|:----:| ------------------------------------------------------------------------- | ------------ |
+| ramfree释放内存           | ✅   | [sbwml/openwrt_pkgs](https://github.com/sbwml/openwrt_pkgs)              | 🟢 已测试    |
+| FileBrowser文件浏览器     | ✅   | [sbwml/openwrt_pkgs](https://github.com/sbwml/openwrt_pkgs)              | 🟢 已测试    |
+| lucky                    | ✅   | [luci-app-lucky](https://github.com/gdy666/luci-app-lucky)               | 🟢 已测试    |
+| openlist2                | ✅   | [luci-app-openlist2](https://github.com/sbwml/luci-app-openlist2)        | 🟢 已测试    |
+| adguardhome              | ✅   | [w9315273/adguardhome](https://github.com/w9315273/luci-app-adguardhome) ; [sirpdboy/adguardhome](https://github.com/sirpdboy/luci-app-adguardhome) | 🟢 已测试    |
+| 其他                     | ⏳   |                                                                          |               |
+
+✅ 支持 ； ⏳ 计划中 ； ❌ 不支持
+
+# 😊Supported devices
+| 设备       | 状态   | 包名                                                                    | 备注               |
+|:----------:|:------:| ----------------------------------------------------------------------- | ------------------ |
+| station-m2 |  ✅    | istoreos-rockchip-armv8-firefly_station-m2-squashfs-sysupgrade.img.gz  | 🟢 已测试          |
+| tvi3315a   |  ✅    | istoreos-rockchip-armv8-tvi_tvi3315a-squashfs-sysupgrade.img.gz        | 🟢 已测试          |
+| jp-tvbox   |  ✅    | istoreos-rockchip-armv8-jp_jp-tvbox-squashfs-sysupgrade.img.gz         | ⭕ 未测试          |
+| 其他       |  ⏳    |                                                                         |                    |
+
+✅ 支持 ； ⏳ 计划中 ； ❌ 不支持
+
+# 🤗Screenshot
+![screenshots](./configfiles/screenshot/screenshot2.png)
+
+# 🌟Star戳一戳，好运加满！😆
+> **"点过 `Star` 的朋友，颜值与智慧双双在线！✨"**
+
+> **"您的每一个⭐️，都是开源土壤里的一缕阳光，让灵感发芽，让创造生长~"**
+
+# 🙏Thanks
+- [istoreos](https://github.com/istoreos/istoreos)
+- [linux-6.6.y](https://github.com/unifreq/linux-6.6.y)
+- [package](https://github.com/Kwonelee/package)
+- [Actions-iStoreOS-RK35XX-24.10](https://github.com/xiaomeng9597/Actions-iStoreOS-RK35XX-24.10)
---
+++ diy/diy-6.x-part2.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+#===============================================
+# Description: DIY script
+# File name: diy-script.sh
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#===============================================
+
+# 集成设备无线
+mkdir -p package/base-files/files/lib/firmware/brcm
+cp -a $GITHUB_WORKSPACE/configfiles/firmware/brcm/* package/base-files/files/lib/firmware/brcm/
+
+# ================================================================
+# 移植RK3399示例，其他RK3399可模仿
+# ================================================================
+# 增加tvi3315a设备
+echo -e "\\ndefine Device/tvi_tvi3315a
+  DEVICE_VENDOR := Tvi
+  DEVICE_MODEL := TVI3315A
+  SOC := rk3399
+  UBOOT_DEVICE_NAME := tvi3315a-rk3399
+endef
+TARGET_DEVICES += tvi_tvi3315a" >> target/linux/rockchip/image/armv8.mk
+
+# 替换package/boot/uboot-rockchip/Makefile
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/Makefile package/boot/uboot-rockchip/Makefile
+
+# 复制dts与配置文件到package/boot/uboot-rockchip
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-tvi3315a.dts} package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3399-tvi3315a-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/tvi3315a-rk3399_defconfig package/boot/uboot-rockchip/src/configs/
+
+# 复制dts到files/arch/arm64/boot/dts/rockchip
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-tvi3315a.dts} target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
+
+# 添加dtb补丁到target/linux/rockchip/patches-6.6
+cp -f $GITHUB_WORKSPACE/configfiles/patch/800-add-rk3399-tvi3315a-dtb-to-makefile.patch target/linux/rockchip/patches-6.6/
+# ================================================================
+# RK3399示例结束
+# ================================================================
+
+# ================================================================
+# 移植RK3566示例，其他RK35xx可模仿
+# ================================================================
+# 增加jp-tvbox设备
+echo -e "\\ndefine Device/jp_jp-tvbox
+\$(call Device/Legacy/rk3566,\$(1))
+  DEVICE_VENDOR := Jp
+  DEVICE_MODEL := JP TVBOX
+  DEVICE_DTS := rk3568/rk3566-jp-tvbox
+  SUPPORTED_DEVICES += jp,jp-tvbox
+  DEVICE_PACKAGES := kmod-scsi-core
+endef
+TARGET_DEVICES += jp_jp-tvbox" >> target/linux/rockchip/image/legacy.mk
+
+# 复制dts到target/linux/rockchip/dts/rk3568
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-jp-tvbox.dts target/linux/rockchip/dts/rk3568/
+# ================================================================
+# RK35xx示例结束
+# ================================================================
+
+
+# ================================================================
+# DIY编译⬇⬇⬇
+# ================================================================
+# 集成core
+mkdir -p files/etc/openclash/core
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+chmod +x files/etc/openclash/core/clash*
+
+# 集成config
+mkdir -p files/etc/config
+wget -qO- https://raw.githubusercontent.com/Kwonelee/Kwonelee/refs/heads/main/rule/openclash > files/etc/config/openclash
+
+# 移除要替换的包
+rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/third_party/luci-app-LingTiGameAcc
+rm -rf feeds/luci/applications/luci-app-filebrowser
+# istoreos-theme
+#rm -rf feeds/third/luci-theme-argon
+#rm -rf feeds/third/luci-app-argon-config
+
+# Set Rust build arg llvm.download-ci-llvm to false.
+RUST_MAKEFILE="feeds/packages/lang/rust/Makefile"
+if [[ -f "${RUST_MAKEFILE}" ]]; then
+  printf "Modifying %s...\n" "${RUST_MAKEFILE}"
+  sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' "${RUST_MAKEFILE}"
+else
+  echo "File ${RUST_MAKEFILE} does not exist." >&2
+fi
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 更新golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+
+# 常见插件
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+git_sparse_clone main https://github.com/gdy666/luci-app-lucky luci-app-lucky lucky
+git_sparse_clone main https://github.com/sbwml/luci-app-openlist2 luci-app-openlist2 openlist2
+git clone -b master https://github.com/w9315273/luci-app-adguardhome package/luci-app-adguardhome
+
+# sbwml/luci-theme-argon
+#git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-theme-argon luci-app-argon-config
+
+# sbwml/openwrt_pkgs
+git_sparse_clone main https://github.com/sbwml/openwrt_pkgs luci-app-ramfree
+git_sparse_clone main https://github.com/Kwonelee/istoreos_pkgs luci-app-filebrowser-go filebrowser
