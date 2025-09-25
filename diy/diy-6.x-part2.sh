d337d7ac6ba3bbb6fd4c2b87b248a04f7f263be0
Kwonelee <995546054@qq.com>
2025-09-25 23:25:03 +0800

    Update diy-6.x-part2.sh

--- diy/diy-6.x-part2.sh
+++ diy/diy-6.x-part2.sh
@@ -111,8 +111,8 @@ git_sparse_clone main https://github.com/gdy666/luci-app-lucky luci-app-lucky lu
 git_sparse_clone main https://github.com/sbwml/luci-app-openlist2 luci-app-openlist2 openlist2
 git clone -b master https://github.com/w9315273/luci-app-adguardhome package/luci-app-adguardhome
 
-# sbwml/luci-theme-argon
-#git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-theme-argon luci-app-argon-config
+# Jaykwok2999/istoreos-theme
+#git_sparse_clone main https://github.com/Jaykwok2999/istoreos-theme luci-theme-argon luci-app-argon-config
 
 # sbwml/openwrt_pkgs
 git_sparse_clone main https://github.com/sbwml/openwrt_pkgs luci-app-ramfree
