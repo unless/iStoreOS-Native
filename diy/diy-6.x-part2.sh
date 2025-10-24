ec9304564cd166efe1fec68524d3fe8e090919f0
unless <jbpv@163.com>
2025-10-24 17:19:02 +0800

    Update diy-6.x-part2.sh

--- diy/diy-6.x-part2.sh
+++ diy/diy-6.x-part2.sh
@@ -46,27 +46,24 @@ cp -f $GITHUB_WORKSPACE/configfiles/patch/800-add-rk3399-tvi3315a-dtb-to-makefil
 # ================================================================
 
 # 增加beikeyun设备
-echo -e "\\ndefine Device/beikeyun
-  DEVICE_VENDOR := Beike
-  DEVICE_MODEL := BeikeYun
-  SOC := rk3328
-  UBOOT_DEVICE_NAME := beikeyun-rk3328
-  SUPPORTED_DEVICES := beikeyun
+echo -e "\\ndefine Device/beikeyun-p1
+$(call Device/Legacy/rk3328,$(1))
+  DEVICE_VENDOR := beikeyun
+  DEVICE_MODEL := p1
+  DEVICE_DTS := rk3328/rk3328-beikeyun-p1
 endef
-TARGET_DEVICES += beikeyun" >> target/linux/rockchip/image/armv8.mk
+TARGET_DEVICES += beikeyun-p1" >> target/linux/rockchip/image/legacy.mk
 
 
 # 复制dts与配置文件到package/boot/uboot-rockchip
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3328/{rk3328.dtsi,rk3328-beikeyun.dts} package/boot/uboot-rockchip/src/arch/arm/dts/
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3328-beikeyun-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/beikeyun-rk3328_defconfig package/boot/uboot-rockchip/src/configs/
+#cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3328/{rk3328.dtsi,rk3328-beikeyun.dts} package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/ubootimage/beikeyun-p1-rk3328-u-boot-rockchip.bin staging_dir/target-aarch64_generic_musl/image/beikeyun-p1-rk3328-u-boot-rockchip.bin
 
 # 复制dts到files/arch/arm64/boot/dts/rockchip
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3328/{rk3328.dtsi,rk3328-beikeyun.dts} target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3328/{rk3328.dtsi,rk3328-beikeyun-p1.dts} target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
 
 # 添加dtb补丁到target/linux/rockchip/patches-6.6
 
-cp -f $GITHUB_WORKSPACE/configfiles/patch/801-add-rk3328-beikeyun-dtb-to-makefile.patch target/linux/rockchip/patches-6.6/
 
 # 增加jp-tvbox设备
 echo -e "\\ndefine Device/jp_jp-tvbox
