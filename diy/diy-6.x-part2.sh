5b89809b0d17c74680bab83ba8562805f1a7797f
unless <jbpv@163.com>
2025-10-26 18:29:10 +0800

    Update diy-6.x-part2.sh

--- diy/diy-6.x-part2.sh
+++ diy/diy-6.x-part2.sh
@@ -60,6 +60,23 @@ cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-jp-tvbox.dts target/linux/
 # RK35xx示例结束
 # ================================================================
 
+# 增加beikeyun设备
+echo -e "\\ndefine Device/beikeyun-p1
+$(call Device/Legacy/rk3328,$(1))
+  DEVICE_VENDOR := beikeyun
+  DEVICE_MODEL := p1
+  DEVICE_DTS := rk3328/rk3328-beikeyun-p1
+endef
+TARGET_DEVICES += beikeyun-p1" >> target/linux/rockchip/image/legacy.mk
+
+# 复制dts到files/arch/arm64/boot/dts/rockchip
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3328/{rk3328.dtsi,rk3328-beikeyun-p1.dts,beikeyun-p1-rk3328-u-boot-rockchip.bin} target/linux/rockchip/dts/rk3328/
+
+sed -i '/^define Build\/Compile$/a\
+\tif echo "$(PROFILE)" | grep -q "beikeyun-p1"; then \\\
+\t\tmkdir -p $(STAGING_DIR_IMAGE); \\\
+\t\tcp -f ../dts/rk3328/beikeyun-p1-rk3328-u-boot-rockchip.bin $(STAGING_DIR_IMAGE)/beikeyun-p1-rk3328-u-boot-rockchip.bin; \\\
+\tfi' Makefile
 
 # ================================================================
 # DIY编译⬇⬇⬇
