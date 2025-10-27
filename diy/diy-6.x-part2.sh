6864c9bb86516e54916238df68670cfbe8b38f36
unless <jbpv@163.com>
2025-10-27 17:33:04 +0800

    Update diy-6.x-part2.sh

--- diy/diy-6.x-part2.sh
+++ diy/diy-6.x-part2.sh
@@ -132,6 +132,8 @@ git_sparse_clone main https://github.com/sbwml/luci-app-openlist2 luci-app-openl
 git clone -b master https://github.com/w9315273/luci-app-adguardhome package/luci-app-adguardhome
 git clone -b master https://github.com/QiuSimons/luci-app-daed package/luci-app-daed
 mkdir -p package/libcron && wget -O package/libcron/Makefile https://raw.githubusercontent.com/immortalwrt/packages/refs/heads/master/libs/libcron/Makefile
+npm install -g pnpm
+
 git clone -b master https://github.com/EasyTier/luci-app-easytier package/luci-app-easytier
 sed -i 's/util.pcdata/xml.pcdata/g' package/luci-app-easytier/luci-app-easytier/luasrc/model/cbi/easytier.lua
 # sbwml/openwrt_pkgs
